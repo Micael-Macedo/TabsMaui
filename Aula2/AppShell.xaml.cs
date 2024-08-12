d1cc4e6fae7e4cafdc32b512dce8cedfe86422b6
Micael-Macedo <micaelm2009@hotmail.com>
2024-08-12 17:12:13 -0300

    Adicione README.md.

---
+++ Aula2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Aula2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Aula2/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Aula2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_hello_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Aviso", "Saudações usuário", "Fechar");
+        }
+    }
+
+}
